7ed88efaf4869c8aba7c1a273297ab857b9e0e8e
Sommer Bareen <sommerbareen1@gmail.com>
2023-03-10 16:44:02 +0500

    🎉 Created typescript react application using vite and added tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App text-center">
+      <h1 className="text-gray-500">Crewmeister - React coding challenge</h1>
+    </div>
+  );
+}
+
+export default App;
